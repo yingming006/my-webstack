90108f361d7df744a536e8f5b3836b2713c4eed5
yingming006 <112357331+yingming006@users.noreply.github.com>
2023-05-16 10:06:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ["/images/logo.svg"]
+    }
+  }
 });
